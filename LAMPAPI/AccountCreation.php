78e141a58d8dc7d41fb64a03f088899941daf433
Kai Doldan <kaidoldan@gmail.com>
2024-09-13 00:20:33 -0400

    Fixing API bugs

---
+++ LAMPAPI/AccountCreation.php
@@ -0,0 +1,45 @@
+<?php
+  $inData = getRequestInfo();
+
+  $firstName = $inData["firstName"];
+  $lastName = $inData["lastName"];
+  $userName = $inData["userName"];
+  $passWord = $inData["passWord"];
+
+
+  $connection = new mysqli("localhost", "Admin", "AdminPassWord", "SmallProject");
+
+  if($connection->connect_error)
+  {
+    returnWithError($connection->connect_error);
+  }
+  else
+  {
+    $stmt = $connection->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $firstName, $lastName, $userName, $passWord);
+    echo 'prepared and bound';
+    $stmt->execute();
+    echo 'executed';
+    $stmt->close();
+    $connection->close();
+    returnWithError("");
+  }
+
+  function sendResultInfoAsJson($obj)
+  {
+    header('Content-type: application/json');
+    echo $obj;
+  }
+
+  function getRequestInfo()
+  {
+    return json_decode(file_get_contents('php://input'), true);
+  }
+
+  function returnWithError($err)
+  {
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+  }
+  
+?>
