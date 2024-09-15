06bc02dc70bb63bd2f963bf8980a99f7091a2eff
Kai Doldan <kaidoldan@gmail.com>
2024-09-15 15:46:16 -0400

    Added deleting contacts

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,3 +1,42 @@
 <?php
-  
+    $inData = getRequestInfo();
+
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $email = $inData["email"];
+    $userID = $inData["userID"];
+
+    $connection = new mysqli("localhost", "Admin", "AdminPassWord", "SmallProject");
+
+    if($connection->connect_error)
+    {
+        returnWithError($connection->connect_error);
+    }
+    else
+    {
+        $stmt = $connection->prepare("DELETE FROM Contacts WHERE firstName=? AND lastName=? AND email=? AND userID=?");
+        $stmt->bind_param("ssss", $firstName, $lastName, $email, $userID);
+        $stmt->execute();
+        $stmt->close();
+        $connection->close();
+        returnWithError("");
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError($err)
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+
 ?>
